4faa7a7f5c79000ed5bf4e2461fd3f3efb4bf956
Ashish Chauhan <90768087+ashish0218groot@users.noreply.github.com>
2024-08-02 15:53:02 +0530

    Add project files.

---
+++ Controllers/HandTrackingController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PythonIntegrated.Services;
+using System.Diagnostics;
+
+namespace PythonIntegrated.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HandTrackingController : ControllerBase
+    {
+        private readonly HandTrackingService _handTrackingService;
+
+        public HandTrackingController(HandTrackingService handTrackingService)
+        {
+            this._handTrackingService = handTrackingService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var handPosition = await _handTrackingService.GetHandPositionAsync();
+            return Ok(handPosition);
+        }
+
+        [HttpGet("start-python-script")]
+        public IActionResult StartPythonScript()
+        {
+            var scriptPath = Path.Combine(Directory.GetCurrentDirectory(), "lib", "hand_tracking.py");
+            var start = new ProcessStartInfo
+            {
+                FileName = "python",
+                Arguments = scriptPath,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true
+            };
+
+            using (var process = Process.Start(start))
+            {
+                var result = process.StandardOutput.ReadToEnd();
+                var error = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                if (!string.IsNullOrEmpty(error))
+                {
+                    return BadRequest(error);
+                }
+
+                return Ok(result);
+            }
+        }
+    }
+}
